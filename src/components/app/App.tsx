d582a65d130240c19de581034f26a3ccda370844
“Olga <strelod@yandex.ru>
2024-01-24 16:36:19 +0300

    feat: organize file structure

---
+++ src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./app.module.css";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.title}>Бургерная</h1>
+    </div>
+  );
+}
+
+export default App;
