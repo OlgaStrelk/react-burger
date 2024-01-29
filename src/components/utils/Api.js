1f1c10b0610933ea019571182595364f02d6d6b5
“Olga <strelod@yandex.ru>
2024-01-29 14:07:29 +0300

    feat: handle fetching errors

--- src/components/utils/Api.js
+++ src/components/utils/Api.js
@@ -1,6 +1,6 @@
 export const getIngredients = () => {
   //   return fetch("https://norma.nomoreparties.space/api/ingredients").then((res)=>(res.ok ? res.json().data : Promise.reject(console.log(res))));
   return fetch("https://norma.nomoreparties.space/api/ingredients").then(
-    (res) => res.json()
+    (res) => (res.ok ? res.json() : Promise.reject(res.status))
   );
 };
