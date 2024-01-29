d52767294b851c831a0a8e9667d7b045dcd5c16c
“Olga <strelod@yandex.ru>
2024-01-29 20:53:45 +0300

    fix: cursor

--- src/components/nav-bar/Navbar.jsx
+++ src/components/nav-bar/Navbar.jsx
@@ -6,8 +6,8 @@ function Navbar({ data }) {
     <nav className={styles.nav}>
       <ul className={styles.list}>
         {data.map((item) => (
-          <li key={item.id} className={`${styles.listItem} mr-2 pt-4 pb-4 pl-5 pr-5`}>
-            <a className={`${styles.link} + ${item.style} `}>
+          <li key={item.id} className={`${styles.listItem} mr-2`}>
+            <a className={`${styles.link} + ${item.style} pt-4 pb-4 pl-5 pr-5`}>
               {item.icon}
               <span className={`${styles.text} ml-2`}>{item.text}</span>
             </a>
