7dd5c4ee23122b6edb76c546c170897181431dd2
“Olga <strelod@yandex.ru>
2024-01-29 20:47:51 +0300

    fix: cursor

--- src/components/nav-bar/Navbar.jsx
+++ src/components/nav-bar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar({ data }) {
           <li key={item.id} className={`${styles.listItem} mr-2 pt-4 pb-4 pl-5 pr-5`}>
             <a className={`${styles.link} + ${item.style} `}>
               {item.icon}
-              <span className="ml-2">{item.text}</span>
+              <span className={`${styles.text} ml-2`}>{item.text}</span>
             </a>
           </li>
         ))}
