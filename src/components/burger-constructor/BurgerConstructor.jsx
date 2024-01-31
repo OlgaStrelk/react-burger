794e244b3b46f20170d0ebbd7d1fc10c465ed1a0
“Olga <strelod@yandex.ru>
2024-01-31 17:27:58 +0300

    fix: constructor indents

--- src/components/burger-constructor/BurgerConstructor.jsx
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -1,71 +1,50 @@
 import {
   ConstructorElement,
   Button,
+  DragIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
 import styles from "./burger-constructor.module.css";
 import Total from "../total/Total";
 function BurgerConstructor({ data }) {
   const img = data[0]?.image;
-  console.log(data)
+  const renderInnerIngredients = () => {
+    return data?.map((item) => (
+      <div key={item._id} className={`${styles.item}`}>
+        <DragIcon type="primary" />
+        <ConstructorElement
+          text={item.name}
+          price={item.price}
+          thumbnail={item.image}
+        />
+      </div>
+    ));
+  };
   return (
     <section className={`${styles.section} mt-25 ml-10`}>
-      <div className={`${styles.wrapper} ${styles.column}`}>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={img}
-        />
-        <div className={`${styles.container}  ${styles.column}`}>
+      <div className="ml-8">
+        <div className={`${styles.wrapper} ${styles.column} ml-8 mb-2`}>
           <ConstructorElement
-            text={data[8]?.name}
-            price={data[8]?.price}
-            thumbnail={data[8]?.image}
-          />
-          <ConstructorElement
-            text={data[14]?.name}
-            price={data[14]?.price}
-            thumbnail={data[14]?.image}
-          />
-          <ConstructorElement
-            text={data[1]?.name}
-            price={data[1]?.price}
-            thumbnail={data[1]?.image}
-          />
-          <ConstructorElement
-            text={data[6]?.name}
-            price={data[6]?.price}
-            thumbnail={data[6]?.image}
-          />
-          <ConstructorElement
-            text={data[2]?.name}
-            price={data[2]?.price}
-            thumbnail={data[2]?.image}
-          />
-          <ConstructorElement
-            text={data[3]?.name}
-            price={data[3]?.price}
-            thumbnail={data[3]?.image}
-          />
-          <ConstructorElement
-            text={data[10]?.name}
-            price={data[10]?.price}
-            thumbnail={data[10]?.image}
-          />
-          <ConstructorElement
-            text={data[13]?.name}
-            price={data[13]?.price}
-            thumbnail={data[13]?.image}
+            type="top"
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={img}
+            className="ml-8"
           />
         </div>
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={200}
-          thumbnail={img}
-        />
+        <div className={`${styles.container}  ${styles.column}`}>
+          {renderInnerIngredients()}
+        </div>
+        <div className="ml-8 mt-2">
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
+            thumbnail={img}
+          />
+        </div>
       </div>
       <div className={`mt-10 ${styles.total}`}>
         <Total />
