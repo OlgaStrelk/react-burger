78a8a7d5edb1cd062ffb1f060891dc8832f41eea
“Olga <strelod@yandex.ru>
2024-02-02 14:08:55 +0300

    feat: Tabbar propsTypes

--- src/components/tabbar/Tabbar.jsx
+++ src/components/tabbar/Tabbar.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
+import { titlesArrayShape } from "../../utils/shapes";
+
 function Tabbar({ data }) {
   const [current, setCurrent] = useState(data[0].value);
 
   return (
     <div style={{ display: "flex" }} className="mt-5">
       {data.map((item) => (
-        <Tab key={item.id}
+        <Tab
+          key={item.id}
           value={item.value}
           active={current === item.value}
           onClick={setCurrent}
@@ -17,5 +21,9 @@ function Tabbar({ data }) {
     </div>
   );
 }
+
+Tabbar.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape(titlesArrayShape)),
+};
 
 export default Tabbar;
