b7b66ae4b8a6b9f013c82ebf8734b77824709539
“Olga <strelod@yandex.ru>
2024-02-04 16:43:55 +0300

    feat: indents for orderModal

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -3,13 +3,13 @@ import styles from './order-details.module.css'
 
 function OrderDetails() {
   return (
-    <>
+    <div className="mt-20 mb-15">
       <span className={styles.digits}>034536</span>
       <p className={styles.description}>идентификатор заказа</p>
       <img src={styles.CheckMarkIcon} />
       <p className={styles.paragraph}>Ваш заказ начали готовить</p>
       <p className={styles.paragraph_inactive}>Дождитесь готовности на орбитальной станции</p>
-    </>
+    </div>
   );
 }
 
