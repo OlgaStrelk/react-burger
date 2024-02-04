d06388abdb715b3a928a26cc7634a4be4746bd86
“Olga <strelod@yandex.ru>
2024-02-04 20:44:04 +0300

    fix: icon render

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -6,7 +6,7 @@ function OrderDetails() {
     <div className="mt-20 mb-15">
       <span className={styles.digits}>034536</span>
       <p className={styles.description}>идентификатор заказа</p>
-      <img src={styles.CheckMarkIcon} />
+      <img src={CheckMarkIcon} />
       <p className={styles.paragraph}>Ваш заказ начали готовить</p>
       <p className={styles.paragraph_inactive}>Дождитесь готовности на орбитальной станции</p>
     </div>
