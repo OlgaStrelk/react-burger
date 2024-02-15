4dfdafc7d062357e6a3a98d214363fbab87d3b7e
“Olga <strelod@yandex.ru>
2024-02-15 19:24:55 +0300

    feat: install dnd & create store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/app/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App";
+import "./index.css";
+import { compose, createStore } from "redux";
+import { Provider } from "react-redux";
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-)
+const enhancer = composeEnhancers();
+
+const store = createStore(rootReducer, enhancer);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
