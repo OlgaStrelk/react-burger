e0b89efbf57a280a45dd7739157bd9b593a53a4f
“Olga <strelod@yandex.ru>
2024-05-15 07:19:01 +0300

    feat: TypedUseSelectorHook

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -1,7 +1,10 @@
-import { useDispatch, useSelector, useStore } from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from 'react-redux'
 import type { AppDispatch, AppStore, RootState } from '../services/store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+// export const useAppSelector = useSelector.withTypes<RootState>()
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
 // export const useAppStore = useStore.withTypes<AppStore>()
