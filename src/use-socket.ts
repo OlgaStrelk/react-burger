9f1cb55ca02ee9b4a05ee062df94e08b24a6deca
“Olga <strelod@yandex.ru>
2024-05-22 14:22:56 +0300

    feat: error handler

--- src/use-socket.ts
+++ src/use-socket.ts
@@ -1,19 +1,19 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from "react";
 
-export const CONNECTING: 'CONNECTING' = 'CONNECTING';
-export const OPEN: 'OPEN' = 'OPEN';
-export const CLOSING: 'CLOSING' = 'CLOSING';
-export const CLOSED: 'CLOSED' = 'CLOSED';
+export const CONNECTING: "CONNECTING" = "CONNECTING";
+export const OPEN: "OPEN" = "OPEN";
+export const CLOSING: "CLOSING" = "CLOSING";
+export const CLOSED: "CLOSED" = "CLOSED";
 
 export const socketStates = {
   0: CONNECTING,
   1: OPEN,
   2: CLOSING,
-  3: CLOSED
+  3: CLOSED,
 };
 
 interface IWSOptions {
-  onMessage: (event: MessageEvent<string>) => void
+  onMessage: (event: MessageEvent<string>) => void;
 
   onConnect?: (event: Event) => void;
   onError?: (event: Event) => void;
@@ -23,43 +23,56 @@ interface IWSOptions {
 export const useSocket = (url: string, options: IWSOptions) => {
   const ws = useRef<WebSocket | null>(null);
 
-  const connect = useCallback((token: string) => {
-    const currentWebsocket = new WebSocket(`${url}?token=${token}`)
-    ws.current =currentWebsocket; 
-    ws.current.onopen = (event: Event) => {
-        if (typeof options.onConnect === 'function') {
+  const connect = useCallback(
+    (token: string) => {
+      const currentWebsocket = new WebSocket(`${url}?token=${token}`);
+      ws.current = currentWebsocket;
+      ws.current.onopen = (event: Event) => {
+        if (typeof options.onConnect === "function") {
           options.onConnect(event);
+        }
+      };
+      ws.current.onerror = (event: Event) => {
+        if (typeof options.onError === "function") {
+          options.onError(event);
         }
       };
 
       ws.current.onclose = (event: Event) => {
-        if (typeof options.onDisconnect === 'function'){options.onDisconnect(event)}
+        if (typeof options.onDisconnect === "function") {
+          options.onDisconnect(event);
+        }
       };
-  }, [url,options]);
-
-  useEffect(
-    () => {
-      if (ws.current) {
-        if (typeof options.onConnect === 'function') {
-          ws.current.onopen = options.onConnect;
-        }
-        if (typeof options.onDisconnect === 'function') {
-          ws.current.onclose = options.onDisconnect;
-        }
-      }
     },
-    [options, ws]
+    [url, options]
   );
 
   useEffect(() => {
+    if (ws.current) {
+      if (typeof options.onConnect === "function") {
+        ws.current.onopen = options.onConnect;
+      }
+      if (typeof options.onDisconnect === "function") {
+        ws.current.onclose = options.onDisconnect;
+      }
+      if (typeof options.onError === "function") {
+        ws.current.onerror = options.onError;
+      }
+    }
+  }, [options, ws]);
+
+  useEffect(() => {
     return () => {
-      if (ws.current && typeof ws.current.close === 'function') {
+      if (ws.current && typeof ws.current.close === "function") {
         ws.current.close();
       }
     };
   }, []);
 
-  const sendData = useCallback((data: { message: string; token: string }) => {}, [ws]);
+  const sendData = useCallback(
+    (data: { message: string; token: string }) => {},
+    [ws]
+  );
 
   return { connect, sendData };
 };
