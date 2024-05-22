971281d0dc397555b59e59bc4f80a18d1197fc79
“Olga <strelod@yandex.ru>
2024-05-22 14:10:57 +0300

    feat: websocket onopen & onclose funcs

--- src/use-socket.ts
+++ src/use-socket.ts
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 export const CONNECTING: 'CONNECTING' = 'CONNECTING';
 export const OPEN: 'OPEN' = 'OPEN';
@@ -26,6 +26,37 @@ export const useSocket = (url: string, options: IWSOptions) => {
   const connect = useCallback((token: string) => {
     const currentWebsocket = new WebSocket(`${url}?token=${token}`)
     ws.current =currentWebsocket; 
+    ws.current.onopen = (event: Event) => {
+        if (typeof options.onConnect === 'function') {
+          options.onConnect(event);
+        }
+      };
+
+      ws.current.onclose = (event: Event) => {
+        if (typeof options.onDisconnect === 'function'){options.onDisconnect(event)}
+      };
+  }, [url,options]);
+
+  useEffect(
+    () => {
+      if (ws.current) {
+        if (typeof options.onConnect === 'function') {
+          ws.current.onopen = options.onConnect;
+        }
+        if (typeof options.onDisconnect === 'function') {
+          ws.current.onclose = options.onDisconnect;
+        }
+      }
+    },
+    [options, ws]
+  );
+
+  useEffect(() => {
+    return () => {
+      if (ws.current && typeof ws.current.close === 'function') {
+        ws.current.close();
+      }
+    };
   }, []);
 
   const sendData = useCallback((data: { message: string; token: string }) => {}, [ws]);
