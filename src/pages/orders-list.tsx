9c8b543cb539e221be3aaaf34c534fd29cde9fc3
“Olga <strelod@yandex.ru>
2024-05-31 14:04:11 +0300

    feat: indents

--- src/pages/orders-list.tsx
+++ src/pages/orders-list.tsx
@@ -1,17 +1,23 @@
 import CardOrder from "../components/card-order/card-order";
 import styles from "./orders-list.module.css";
+import OrdersPanel from "../components/orders-panel/orders-panel";
 
 function OrdersListPage() {
   return (
-    <>
+    <main className={styles.main}>
       <h1 className={styles.title}>Лента заказов</h1>
-      <main className={styles.main}>
+
+      <div className={styles.container}>
         <section className={styles.section}>
+          <CardOrder /> <CardOrder />
+          <CardOrder />
+          <CardOrder />
+          <CardOrder />
           <CardOrder />
         </section>
-        <section className={styles.section}></section>
-      </main>
-    </>
+        <OrdersPanel />
+      </div>
+    </main>
   );
 }
 
