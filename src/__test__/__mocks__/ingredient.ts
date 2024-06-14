3741d87873a0b7cca466d27a60221bfa3044719c
“Olga <strelod@yandex.ru>
2024-06-14 16:52:43 +0300

    feat: add ingredient successful test

---
+++ src/__test__/__mocks__/ingredient.ts
@@ -0,0 +1,33 @@
+import { TConstructorIngredient } from "../../utils/types";
+
+export const BUN_INGREDIENT: TConstructorIngredient = {
+  _id: "643d69a5c3f7b9001cfa093c",
+  name: "Краторная булка N-200i",
+  type: "bun",
+  proteins: 80,
+  fat: 24,
+  carbohydrates: 53,
+  calories: 420,
+  price: 1255,
+  image: "https://code.s3.yandex.net/react/code/bun-02.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+  quantity: 0,
+  id: "djasbdjsad",
+};
+
+export const FILLING_INGREDIENT: TConstructorIngredient = {
+  _id: "643d69a5c3f7b9001cfa093e",
+  name: "Филе Люминесцентного тетраодонтимформа",
+  type: "main",
+  proteins: 44,
+  fat: 26,
+  carbohydrates: 85,
+  calories: 643,
+  price: 988,
+  image: "https://code.s3.yandex.net/react/code/meat-03.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/meat-03-large.png",
+  quantity: 0,
+  id: "djasbdjsad",
+};
--- src/services/reducers/burger-constructor.test.ts
+++ src/services/reducers/burger-constructor.test.ts
@@ -1,4 +1,33 @@
 import { expect, test } from "vitest";
+import {
+  ConstructorState,
+  constructorReducer as reducer,
+} from "./burger-constructor";
+import { ADD_INGREDIENT } from "../constants/ingredients";
+import {
+  FILLING_INGREDIENT,
+  BUN_INGREDIENT,
+} from "../../__test__/__mocks__/ingredient";
 test("init", () => {
   expect(true).toBeTruthy();
+});
+const initialState = {
+  addedIngredients: {
+    buns: null,
+    ingredients: [],
+  },
+};
+test("should handle a filling ingredient being added to constructor", () => {
+  const previousState: ConstructorState = initialState;
+  const action = reducer(previousState, {
+    type: ADD_INGREDIENT,
+    payload: FILLING_INGREDIENT,
+  });
+  const expectedState = {
+    addedIngredients: {
+      buns: null,
+      ingredients: [FILLING_INGREDIENT],
+    },
+  };
+  expect(action).toEqual(expectedState);
 });
