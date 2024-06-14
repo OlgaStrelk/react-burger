3fe10a4e6ce88adb86ed3ae630cdd397490a72de
“Olga <strelod@yandex.ru>
2024-06-14 16:55:23 +0300

    feat: describe

--- src/services/reducers/burger-constructor.test.ts
+++ src/services/reducers/burger-constructor.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { expect, test, describe } from "vitest";
 import {
   ConstructorState,
   constructorReducer as reducer,
@@ -8,26 +8,26 @@ import {
   FILLING_INGREDIENT,
   BUN_INGREDIENT,
 } from "../../__test__/__mocks__/ingredient";
-test("init", () => {
-  expect(true).toBeTruthy();
-});
+
 const initialState = {
   addedIngredients: {
     buns: null,
     ingredients: [],
   },
 };
-test("should handle a filling ingredient being added to constructor", () => {
-  const previousState: ConstructorState = initialState;
-  const action = reducer(previousState, {
-    type: ADD_INGREDIENT,
-    payload: FILLING_INGREDIENT,
+describe("burger constructor reducer", () => {
+  test("should handle a filling ingredient being added to constructor", () => {
+    const previousState: ConstructorState = initialState;
+    const action = reducer(previousState, {
+      type: ADD_INGREDIENT,
+      payload: FILLING_INGREDIENT,
+    });
+    const expectedState = {
+      addedIngredients: {
+        buns: null,
+        ingredients: [FILLING_INGREDIENT],
+      },
+    };
+    expect(action).toEqual(expectedState);
   });
-  const expectedState = {
-    addedIngredients: {
-      buns: null,
-      ingredients: [FILLING_INGREDIENT],
-    },
-  };
-  expect(action).toEqual(expectedState);
 });
